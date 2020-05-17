1edcbe2a1744e16d4c4a6bcae3c37e0227a1de4a
jessicaireland <60513895+jessicaireland@users.noreply.github.com>
2020-05-17 12:20:59 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <div className="header">
+   <h1>Do Dates</h1>
+   </div>
+   <main className="main-page">
+     <ul className="nav-bar-side">
+       <li>Lists</li>
+       <li>Calendar</li>
+       <li>Planner</li>
+       <li>Reminders</li>
+       <li>Contacts</li>
+     </ul>
+  
+    <div className="main-list">
+      <h3>Tasks</h3>
+      <ul>
+        <li>Call Doctor</li>
+        <li>Make List of Baby Names</li>
+        <li>Make List of Baby Stuff Needed</li>
+      </ul>
+      <button type="button" className="btn-add">Add To List</button>
+      <h3>Girl Names</h3>
+      <ul>
+        <li>Julie</li>
+        <li>Kaya</li>
+        <li>Lilly</li>
+      </ul>
+      <button type="button" className="btn-add">Add To List</button>
+      <h3>Boy Names</h3>
+      <ul>
+        <li>Toby</li>
+        <li>Alphie</li>
+        <li>Kyle</li>
+      </ul>
+      <button type="button" className="btn-add">Add To list</button>
+
     </div>
+   </main>
+
+
+   </>
   );
 }
 
