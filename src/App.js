226f55ebf1b416e66a9bf84a026c2d1cb75ff094
jessicaireland <60513895+jessicaireland@users.noreply.github.com>
2020-05-19 09:14:47 -0400

    props and components

--- src/App.js
+++ src/App.js
@@ -1,47 +1,87 @@
 import React from 'react';
-// import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import ToDoMain from "./components/ToDoMain";
+import GirlNames from './components/GirlNames';
 
 
+// import logo from './logo.svg';
+// const images = {
+  // logo: require("./images/37616615-pregnant-woman-symbol-stylized-vector-sketch.jpg"),
+ 
+// }
 
-function App() {
+const user = {
+  name: "Sally Lou",
+  avatarImage: require("./images/myAvatar.png"),
+}
+
+const toDoLists = [{
+  id: 1, 
+  item: "Make Doctors Appointment",
+  description: "call Dr",
+  completed: false
+},
+{
+  id: 2,
+  item: "Make List of Girl Names",
+  description: "List of girl names",
+  completed: false
+},
+{
+  id: 3,
+  item: "Make List of Boy Names",
+  description: "List of boy names",
+  completed: false
+},
+{
+  id: 4,
+  item: "Make List of Baby Things Needed",
+  description: "what are the things needed for baby",
+  completed: false
+}];
+
+const girlNames = [{
+  id: 1,
+  name: "Julie"
+},
+{
+  id: 2,
+  name: "Kaya"
+},
+{
+  id: 3,
+  name: "Lilly"
+}]
+
+
+function App() { 
   return (
-    <>
-    
-    <div className="header">
-   <h1>Do Dates</h1>
-   </div>
+    <> 
+   <Header />
+
    <main className="main-page">
-     <ul className="nav-bar-side">
-       <li>Lists</li>
-       <li>Calendar</li>
-       <li>Planner</li>
-       <li>Reminders</li>
-       <li>Contacts</li>
-     </ul>
+
+   <SideBar  user={user}/>  
   
     <div className="main-list">
-      <h3>Tasks</h3>
+      <h3>Starting Tasks</h3>
       <ul>
-        <li>Call Doctor</li>
-        <li>Make List of Baby Names</li>
-        <li>Make List of Baby Stuff Needed</li>
+        {toDoLists.map((toDoLists, index) =>
+        <ToDoMain toDoLists={toDoLists} key={index}/>
+        )}
       </ul>
       <button type="button" className="btn-add">Add To List</button>
       <h3>Girl Names</h3>
       <ul>
-        <li>Julie</li>
-        <li>Kaya</li>
-        <li>Lilly</li>
+        {girlNames.map((girlNames, index) =>
+        <GirlNames girlNames={girlNames} key={index}/>
+        )}
+        
       </ul>
       <button type="button" className="btn-add">Add To List</button>
-      <h3>Boy Names</h3>
-      <ul>
-        <li>Toby</li>
-        <li>Alphie</li>
-        <li>Kyle</li>
-      </ul>
-      <button type="button" className="btn-add">Add To list</button>
+    
 
     </div>
    </main>
